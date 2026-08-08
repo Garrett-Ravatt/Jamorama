78a328580d66c11a3ab07520788186b1a5fd0fd3
BT100 Building <BT100@ac.sdmesa.net>
2026-08-08 12:41:47 -0700

    adding soul_data resources

---
+++ Character Blocks/soul_data.gd
@@ -0,0 +1,15 @@
+
+
+class_name SoulData
+
+extends Resource
+
+@export var sins: Array[String] = []
+@export var virtues: Array[String] = []
+@export var id = 0
+@export var name = ""
+
+
+func dialogue(num):
+	num+=1
+	
