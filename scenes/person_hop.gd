d52be084b8d67cad74a02180cb2b6dba4305dc94
BT100 Building <BT100@ac.sdmesa.net>
2026-08-09 11:27:36 -0700

    changed person logic

--- scenes/person_hop.gd
+++ scenes/person_hop.gd
@@ -9,10 +9,10 @@ func _process(delta: float) -> void:
 	position.y -= velocity
 	velocity -= deceleration
 	position.x += velocity2
-	if position.y >= 0:
+	if position.y >= 71:
 		velocity = 0
-		position.y = 0
-	if position.y == 0:
+		position.y = 71
+	if position.y == 71:
 		if rand.randf() > .99:
 			velocity = .9
 	velocity2 += rand.randf_range(-.15,.15)
@@ -22,8 +22,8 @@ func _process(delta: float) -> void:
 		velocity2 = -.99
 	
 	
-	if position.x > 1000:
-		position.x = 1000
+	if position.x > 1900:
+		position.x = 1900
 		
 	if position.x < 0:
 		position.x = 0
--- scenes/slot.gd
+++ scenes/slot.gd
@@ -15,12 +15,13 @@ func _drop_data(at_position: Vector2, data: Variant) -> void:
 	peopleImages[people] = TextureRect.new()
 	peopleImages[people].texture = newTexture 
 	peopleImages[people].pivot_offset_ratio = Vector2(.5,1)
-	peopleImages[people].position.x += rand.randf_range(-500,500)
-	peopleImages[people].anchor_top = 0
-	peopleImages[people].anchor_left = 0.5
-	peopleImages[people].anchor_right = .5
-	peopleImages[people].anchor_bottom= 1
-	peopleImages[people].scale = Vector2(.3,.3)
+	peopleImages[people].position.x += rand.randf_range(0,1900)
+	#peopleImages[people].anchor_top = 0
+	#peopleImages[people].anchor_left = 0.5
+	#peopleImages[people].anchor_right = .5
+	#peopleImages[people].anchor_bottom= 1
+	peopleImages[people].position.y = 71
+	peopleImages[people].scale = Vector2(1,1)
 	peopleImages[people].set_script(load("res://scenes/person_hop.gd"))
 	
 	add_child(peopleImages[people])
