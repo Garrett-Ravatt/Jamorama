9ccf3d41aa1e04073da3a758eaa94b0adc768bc1
garrett-ravatt <garrbear123456789@gmail.com>
2026-08-10 01:29:58 -0700

    audio fix

--- scenes/plea_scene.gd
+++ scenes/plea_scene.gd
@@ -1,4 +1,16 @@
+@tool
 extends Node2D
+
+@export_tool_button("import streams", "Callable") var setup_action = setup
+func setup():
+	voice_streams.clear()
+	var dirp := "res://audio/"
+	for path in DirAccess.open(dirp).get_files():
+		if path.contains(".import"):
+			continue
+		var stream : AudioStream = load(dirp+path)
+		voice_streams.append(stream)
+		print(path)
 
 @export var voice_streams : Array[AudioStream]
 
@@ -7,6 +19,7 @@ func _ready() -> void:
 	for stream in voice_streams:
 		if stream != null:
 			DialogueDump.audio_streams[stream.resource_path] = stream
+	print(DialogueDump.audio_streams.keys())
 			
 	DialogueDump.ab = %AngelBalloon
 	DialogueDump.pb = %PleaBalloon
